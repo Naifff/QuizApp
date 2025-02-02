e15847cb5996c56cdddefabb8bc0668616dec5b6
janettef <knifffe@yandex.ru>
2025-02-02 21:33:03 +0300

    add txt

--- src/main/java/org/example/quizapp/domain/QuestionType.java
+++ src/main/java/org/example/quizapp/domain/QuestionType.java
@@ -1,7 +1,7 @@
 package org.example.quizapp.domain;
 
 public enum QuestionType {
-	SINGLE_CHOICE,
-	MULTIPLE_CHOICE,
-	TEXT
+	SINGLE_CHOICE,  // Один правильный ответ (радиокнопки)
+	MULTIPLE_CHOICE, // Несколько правильных ответов (чекбоксы)
+	TEXT // Вопрос с текстовым вводом
 }
